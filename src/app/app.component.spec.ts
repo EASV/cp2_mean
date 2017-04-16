68697501d46cb34eb3f95e5958eff61edde6f80a
lbilde <ljb@easv.dk>
2017-04-16 23:53:10 +0200

    added failing unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,16 +17,31 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'CoursePlanner2'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('CoursePlanner2');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    const app = fixture.debugElement.componentInstance;
+    expect(compiled.querySelector('h1').textContent).toContain(app.title);
+  }));
+
+  it(`should have as description 'Welcome to the new and improved CoursePlanner2'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Welcome to the new and improved CoursePlanner2');
+  }));
+
+  it('should render description in a h4 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const app = fixture.debugElement.componentInstance;
+    expect(compiled.querySelector('h4').textContent).toContain(app.description);
   }));
 });
