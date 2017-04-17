cef4752be89276860d0dc95e271806e7bcd3884a
lbilde <ljb@easv.dk>
2017-04-17 22:46:32 +0200

    added course to CourseComponent and added test for next session

--- src/app/courses/course/course.component.spec.ts
+++ src/app/courses/course/course.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseComponent } from './course.component';
+import {Course} from '../course';
 
 describe('CourseComponent', () => {
   let component: CourseComponent;
@@ -22,4 +23,24 @@ describe('CourseComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have a Course object`, async(() => {
+    component.ngOnInit();
+    expect(component.course).toBeDefined();
+  }));
+
+  it(`Adding a course should provide a h3, h4 and p tag`, async(() => {
+    const compiled = this.fixture.debugElement.nativeElement;
+    this.component.ngOnInit();
+    this.fixture.detectChanges();
+    this.component.course = {
+      id: 'jswebapps',
+      title: 'JS Web Apps'
+    };
+    this.fixture.detectChanges();
+    expect(compiled.getElementsByTagName('h3').length === 1 &&
+            compiled.getElementsByTagName('h4').length === 1 &&
+            compiled.getElementsByTagName('p').length === 1
+    ).toBeTruthy();
+  }));
 });
--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Course} from '../course';
 
 @Component({
   selector: 'app-course',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
+  course: Course;
+
   constructor() { }
 
   ngOnInit() {
+    this.course = <Course> {};
   }
 
 }
