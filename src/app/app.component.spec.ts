97b92676ee5d50a6e283dac04f9e3f8da8b45162
lbilde <ljb@easv.dk>
2017-04-17 21:36:47 +0200

    adding failing unit test ngfor part2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,51 +4,64 @@ import { AppComponent } from './app.component';
 import Any = jasmine.Any;
 
 describe('AppComponent', () => {
+  let fixture;
+  let component: AppComponent;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+    this.fixture = TestBed.createComponent(AppComponent);
+    this.component = this.fixture.debugElement.componentInstance;
+
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(this.component).toBeTruthy();
   }));
 
   it(`should have as title 'CoursePlanner2'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('CoursePlanner2');
+    expect(this.component.title).toEqual('CoursePlanner2');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const app = fixture.debugElement.componentInstance;
-    expect(compiled.querySelector('h1').textContent).toContain(app.title);
+    this.fixture.detectChanges();
+    const compiled = this.fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(this.component.title);
   }));
 
   it(`should have as description 'Welcome to the new and improved CoursePlanner2'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.description).toEqual('Welcome to the new and improved CoursePlanner2');
+    expect(this.component.description).toEqual('Welcome to the new and improved CoursePlanner2');
   }));
 
   it('should render description in a h4 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const app = fixture.debugElement.componentInstance;
-    expect(compiled.querySelector('h4').textContent).toContain(app.description);
+    this.fixture.detectChanges();
+    const compiled = this.fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h4').textContent).toContain(this.component.description);
   }));
 
   it(`should have a Array of course object`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.courses instanceof Array).toBe(true);
+    this.component.ngOnInit();
+    expect(this.component.courses instanceof Array).toBe(true);
+  }));
+
+  it(`Adding 3 courses to Array should provide 3 p tags`, async(() => {
+    const compiled = this.fixture.debugElement.nativeElement;
+    this.component.ngOnInit();
+    this.fixture.detectChanges();
+    this.component.courses = [
+      {
+        title: 'JS Web Apps'
+      },
+      {
+        title: 'Android'
+      },
+      {
+        title: 'Demo day'
+      }
+    ];
+    this.fixture.detectChanges();
+    expect(compiled.getElementsByTagName('p').length === 3).toBe(true);
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  courses: Array<any>;
   title = 'CoursePlanner2';
   description = 'Welcome to the new and improved CoursePlanner2';
+
+  ngOnInit() {
+    this.courses = [];
+  }
 }
