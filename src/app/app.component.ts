bd218605f14171cd705f9d1a38354bb394e9fcc7
lbilde <ljb@easv.dk>
2017-05-03 22:26:59 +0200

    Unsubscribing a subscription that gets data from the Ret API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Course} from './courses/course';
 import {CourseService} from './courses/course.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   courses: Array<Course>;
   title = 'CoursePlanner2';
   description = 'Welcome to the new and improved CoursePlanner2';
-
+  courseServiceSub: Subscription;
   constructor(private courseService: CourseService) { }
 
   ngOnInit() {
-    this.courseService.readAll().subscribe(courses => {
+    this.courseServiceSub = this.courseService.readAll().subscribe(courses => {
       this.courses = courses;
     });
+  }
+
+  ngOnDestroy() {
+    if (this.courseServiceSub) {
+      this.courseServiceSub.unsubscribe();
+    }
   }
 }
